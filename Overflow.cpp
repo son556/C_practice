b8497af7fffc9dfb5ae0445b8d6651ad2f126ec2
son556 <shhmun02@naver.com>
2022-07-13 20:27:57 +0900

    2022-07-13

---
+++ Overflow.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(void)
+{
+	int x = INT_MAX;
+	printf("int 형의 최댓값 x 는 %d 입니다.", x);
+	return 0;
+}
---
+++ twovariable.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x = 30;
+	int y = 20;
+	printf("x = %d\n",x);
+	printf("y = %d\n",y);
+	printf("x + y = %d\n",x + y);
+	printf("x - y = %d\n",x - y);
+	printf("x * y = %d\n",x * y);
+	printf("x / y = %d\n",x / y);// 두개의 정수를 나누기 연산한 후 %d 로 표현하면
+//	몫만 출력 됨 
+}
